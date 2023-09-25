ecfed68e678416fba89e1477260b40e68f05d6e7
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-09-25 20:34:26 +0300

    Revert "add divide with scale"
    This reverts commit 0ceb44a27a6a2fe80ea4d198a4a57b48d9dfd51f.

--- src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
+++ src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
@@ -8,8 +8,5 @@ public class CalculatorApp {
         var calc = new CalculatorImpl();
         var resDivide1 = calc.divide(BigDecimal.valueOf(1.0), BigDecimal.valueOf(7.0));
         System.out.println("1 / 7 = " + resDivide1);
-
-        var resDivide2 = calc.divide(BigDecimal.valueOf(1), BigDecimal.valueOf(6), 5);
-        System.out.println("1 / 6 = " + resDivide2);
     }
 }
