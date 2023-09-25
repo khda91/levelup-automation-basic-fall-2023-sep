0244b43fc5f2ddcb20dc2a3f60943d2ba39899b7
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-09-25 20:10:48 +0300

    Add Calucalor realisation. Add Calculator subtract and multiply methods

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -5,4 +5,8 @@ import java.math.BigDecimal;
 public interface Calculator {
 
     BigDecimal add(BigDecimal a, BigDecimal b);
+
+    BigDecimal subtract(BigDecimal a, BigDecimal b);
+
+    BigDecimal multiply(BigDecimal a, BigDecimal b);
 }
---
+++ src/main/java/ru/levelp/at/lesson02/git/CalculatorImpl.java
@@ -0,0 +1,20 @@
+package ru.levelp.at.lesson02.git;
+
+import java.math.BigDecimal;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public BigDecimal add(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    @Override
+    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    @Override
+    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+}
