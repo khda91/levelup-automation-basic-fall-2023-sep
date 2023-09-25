ea18c73dcdab35c954b7b3c105377f3dcb54be0d
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-09-25 21:27:21 +0300

    Change examples

--- src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
+++ src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
@@ -9,11 +9,14 @@ public class CalculatorApp {
         var resDivide1 = calc.divide(BigDecimal.valueOf(1.0), BigDecimal.valueOf(7.0));
         System.out.println("1 / 7 = " + resDivide1);
 
+        var resDivide4 = calc.divide(BigDecimal.valueOf(1.0), BigDecimal.valueOf(7.0));
+        System.out.println("1 / 7 = " + resDivide4);
+
         var resDivide2 = calc.divide(BigDecimal.valueOf(1), BigDecimal.valueOf(6), 5);
         System.out.println("1 / 6 = " + resDivide2);
 
-        var resDivide3 = calc.divide(BigDecimal.valueOf(2), BigDecimal.valueOf(6), 5);
-        System.out.println("2 / 6 = " + resDivide3);
+        var resMultiplyArray1 = calc.multiply(BigDecimal.valueOf(1), BigDecimal.valueOf(6), BigDecimal.valueOf(3));
+        System.out.println("1 * 6 * 3 = " + resMultiplyArray1);
 
         var resAddArray1 = calc.add(BigDecimal.valueOf(1), BigDecimal.valueOf(6), BigDecimal.valueOf(3));
         System.out.println("1 + 6 + 3 = " + resAddArray1);
