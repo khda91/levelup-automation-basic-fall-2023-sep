552a12d7b5e73fb73ad54ae72d22841d15a605ca
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-09-25 21:20:41 +0300

    added divide example

--- src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
+++ src/main/java/ru/levelp/at/lesson02/git/CalculatorApp.java
@@ -12,6 +12,9 @@ public class CalculatorApp {
         var resDivide2 = calc.divide(BigDecimal.valueOf(1), BigDecimal.valueOf(6), 5);
         System.out.println("1 / 6 = " + resDivide2);
 
+        var resDivide3 = calc.divide(BigDecimal.valueOf(2), BigDecimal.valueOf(6), 5);
+        System.out.println("2 / 6 = " + resDivide3);
+
         var resAddArray1 = calc.add(BigDecimal.valueOf(1), BigDecimal.valueOf(6), BigDecimal.valueOf(3));
         System.out.println("1 + 6 + 3 = " + resAddArray1);
     }
