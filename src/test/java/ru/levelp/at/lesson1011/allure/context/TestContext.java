93e63ab4b25e714fd67a50f29d7a6f321e1a40e8
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-10-23 22:00:44 +0300

    Lesson 10 - CI/CD. Allure
    - allure
    - allure set  up
    - allure test description
    - allure steps
    - allure attachment
    - junit 5 test callbacks

---
+++ src/test/java/ru/levelp/at/lesson1011/allure/context/TestContext.java
@@ -0,0 +1,30 @@
+package ru.levelp.at.lesson1011.allure.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class TestContext {
+
+    private static TestContext instance;
+
+    private final Map<String, Object> context;
+
+    private TestContext() {
+        context = new HashMap<>();
+    }
+
+    public void add(String key, Object value) {
+        context.put(key, value);
+    }
+
+    public Object get(String key) {
+        return context.get(key);
+    }
+
+    public static TestContext getInstance() {
+        if (instance == null) {
+            instance = new TestContext();
+        }
+        return instance;
+    }
+}
