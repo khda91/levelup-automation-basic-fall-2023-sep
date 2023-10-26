f4566a34308301d3c30cd1a42083e8fa977acde8
Khodakovskiy Dmitriy <khda91@gmail.com>
2023-10-26 20:46:36 +0300

    Lesson 12 - CI/CD. Allure
    - Jenkins

---
+++ src/test/java/ru/levelp/at/lesson1011/cicd/extension/AllureAttachmentCallback.java
@@ -0,0 +1,32 @@
+package ru.levelp.at.lesson1011.cicd.extension;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import ru.levelp.at.lesson1011.cicd.context.TestContext;
+
+public class AllureAttachmentCallback implements AfterTestExecutionCallback {
+
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) {
+        System.out.println("AllureAttachmentCallback");
+        extensionContext.getExecutionException().ifPresent(throwable -> {
+            var driver = (WebDriver) TestContext.getInstance().get("driver");
+            attachScreenshot(driver);
+            attachPageSource(driver);
+        });
+    }
+
+    @Attachment(value = "image", type = "image/png", fileExtension = "png")
+    private byte[] attachScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    private void attachPageSource(WebDriver driver) {
+        Allure.addAttachment("page_source", "text/html", driver.getPageSource(), ".html");
+    }
+}
